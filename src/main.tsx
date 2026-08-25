a2884d8c00c379154851d13628042d89e19dfe4f
Danijel Jovanović <danijel.xda@gmail.com>
2026-08-25 10:25:19 +0200

    feat(ui): wrap editor in FluentProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { FluentProvider, webLightTheme } from '@fluentui/react-components'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={webLightTheme} style={{ height: '100vh' }}>
+      <App />
+    </FluentProvider>
   </React.StrictMode>,
 )
