5216189cd2dfd580fa9b65b166a7eb25dff6a1d4
Danijel Jovanović <danijel.xda@gmail.com>
2026-08-25 12:27:58 +0200

    chore: install Fluent icons in Unix launcher

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if ! command -v node >/dev/null 2>&1; then
   exit 1
 fi
 
-if [ ! -d node_modules/@fluentui/react-components ]; then
+if [ ! -d node_modules/@fluentui/react-components ] || [ ! -d node_modules/@fluentui/react-icons ]; then
   echo "Instaliram ili osvežavam zavisnosti..."
   npm install --no-audit --no-fund
 fi
