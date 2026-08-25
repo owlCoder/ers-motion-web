00a35b3d174a5493bd03cac62d5c4fc2d49646d7
Danijel Jovanović <danijel.xda@gmail.com>
2026-08-25 10:34:24 +0200

    fix(ui): normalize root layout after Fluent migration

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { FluentProvider, webLightTheme } from '@fluentui/react-components'
 import App from './App'
 
+document.documentElement.style.height = '100%'
+document.body.style.margin = '0'
+document.body.style.height = '100%'
+document.body.style.overflow = 'hidden'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <FluentProvider theme={webLightTheme} style={{ height: '100vh' }}>
